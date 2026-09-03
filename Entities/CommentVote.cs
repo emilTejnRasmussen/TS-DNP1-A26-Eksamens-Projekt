f1390ff7a686ec530aa9c5826ecacf366ab0260e
Emil Tejn Rasmussen <emil@rasie.dk>
2026-09-03 17:01:48 +0200

    create commentVote entity

---
+++ Entities/CommentVote.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities;
+
+public class CommentVote(int id, int userId, int commentId, VoteType voteType)
+{
+    public int Id { get; set; } = id;
+    public int UserId { get; set; } = userId;
+    public int CommentId { get; set; } = commentId;
+    public VoteType VoteType { get; set; } = voteType;
+}
