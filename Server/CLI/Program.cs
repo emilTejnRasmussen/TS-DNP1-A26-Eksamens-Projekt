8797f7d032ca188d50f987a63689a36279d6979b
Emil Tejn Rasmussen <emil@rasie.dk>
2026-09-17 11:00:41 +0200

    Refactor CLI app to use file repoes

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -1,15 +1,16 @@
 ﻿using CLI.UI;
+using FileRepositories;
 using InMemoryRepositories;
 using RepositoryContract;
 
 Console.WriteLine("Starting CLI app...");
 
-IUserRepository userRepository = new UserInMemoryRepository();
-ICommentRepository commentRepository = new CommentInMemoryRepository();
-IPostRepository postRepository = new PostInMemoryRepository();
-ISubforumRepository subforumRepository = new SubforumInMemoryRepository();
-ICommentVoteRepository commentVoteRepository = new CommentVoteInMemoryRepository();
-IPostVoteRepository postVoteRepository = new PostVoteInMemoryRepository();
+IUserRepository userRepository = new UserFileRepository();
+ICommentRepository commentRepository = new CommentFileRepository();
+IPostRepository postRepository = new PostFileRepository();
+ISubforumRepository subforumRepository = new SubforumFileRepository();
+ICommentVoteRepository commentVoteRepository = new CommentVoteFileRepository();
+IPostVoteRepository postVoteRepository = new PostVoteFileRepository();
 
 CliApp cliApp = new(userRepository, commentRepository, postRepository, subforumRepository, commentVoteRepository, postVoteRepository);
 await cliApp.StartAsync();
